32a118241a39895d09a751331f5b213372580e15
eldhosby007 <eldhosby007@gmail.com>
2024-06-06 23:10:17 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+// `https://api.frankfurter.app/latest?amount=100&from=EUR&to=USD`
 
-function App() {
+import { useEffect, useState } from "react";
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <CurrencyConversion />
     </div>
   );
 }
 
-export default App;
+function CurrencyConversion() {
+  const [curencyInput, setCurrencyInput] = useState(1);
+  const [fromCurrency, setFromCurrency] = useState("USD");
+  const [toCurrency, setToCurrency] = useState("INR");
+  const [outputValue, setOutputValue] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(
+    function () {
+      async function getCurrencyValue() {
+        setIsLoading(true);
+        const res = await fetch(
+          `https://api.frankfurter.app/latest?amount=${curencyInput}&from=${fromCurrency}&to=${toCurrency}`
+        );
+        const result = await res.json();
+        setOutputValue(result.rates[toCurrency]);
+        setIsLoading(false);
+      }
+      if (fromCurrency === toCurrency) return setOutputValue(curencyInput);
+      getCurrencyValue();
+    },
+    [curencyInput, fromCurrency, toCurrency]
+  );
+  return (
+    <>
+      {" "}
+      <input
+        type="text"
+        value={curencyInput}
+        onChange={(e) => setCurrencyInput(Number(e.target.value))}
+        disabled={isLoading}
+      />
+      <select
+        value={fromCurrency}
+        onChange={(e) => setFromCurrency(e.target.value)}
+        disabled={isLoading}
+      >
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
+      </select>
+      <select
+        value={toCurrency}
+        onChange={(e) => setToCurrency(e.target.value)}
+        disabled={isLoading}
+      >
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
+      </select>
+      <p>
+        {outputValue} {toCurrency}
+      </p>
+    </>
+  );
+}
